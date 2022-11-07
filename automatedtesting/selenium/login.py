dab8ffa32f6738b31431ef6e1bb71171af3ebdd8
Duong Anh Duc <duongduc98@gmail.com>
2022-11-07 14:22:31 +0700

    update code

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -1,18 +1,29 @@
 # #!/usr/bin/env python
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-
+from datetime import datetime
 
 # Start the browser and login with standard_user
+
 def login (user, password):
     print ('Starting the browser...')
-    # --uncomment when running in Azure DevOps.
-    # options = ChromeOptions()
-    # options.add_argument("--headless") 
-    # driver = webdriver.Chrome(options=options)
-    driver = webdriver.Chrome()
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
     print ('Browser started successfully. Navigating to the demo page to login.')
     driver.get('https://www.saucedemo.com/')
+    input_username = driver.find_element(By.ID, 'user-name')
+    input_password = driver.find_element(By.ID, 'password')
+    btn_login = driver.find_element(By.ID, 'login-button')
 
-login('standard_user', 'secret_sauce')
+    input_username.send_keys(user)
+    input_password.send_keys(password)
+    btn_login.click()
 
+    product_label = driver.find_element(By.XPATH, "//*[@id='header_container']/div[@class='header_secondary_container']/span[@class='title']")
+    assert product_label.text == 'PRODUCTS'
+
+    print('{}: Login with username {} and password {} successfully'.format(datetime.now(), user, password))
+
+    return driver
--- terraform/environments/test/terraform.tfvars
+++ terraform/environments/test/terraform.tfvars
@@ -1,15 +1,19 @@
 # Azure subscription vars
-subscription_id = ""
-client_id = ""
-client_secret = ""
-tenant_id = ""
+subscription_id = "f86c8aef-eed2-4250-a0a6-3dd45ecd4082"
+client_id = "d7da08fd-3247-49b3-8ef8-745ebd343f82"
+client_secret = "tsd8Q~kYP5ZWKyR14WFYvQjtapzX6GmKi0hOMbrk"
+tenant_id = "7ed19845-af0a-4369-bd19-d67db8f50d3c"
 
 # Resource Group/Location
-location = ""
-resource_group_name = ""
-application_type = ""
+location = "East US"
+resource_group_name = "Azuredevops"
+application_type = "ducda"
 
 # Network
-virtual_network_name = ""
+virtual_network_name = "myVirtualNetwork"
 address_space = ["10.5.0.0/16"]
 address_prefix_test = "10.5.1.0/24"
+
+# Virtual machine
+admin_username = "ducda177"
+admin_password = "MyStr00ngP@$$w0rD"
